14f9da7802e55cb287b8199af9342f211dc4c886
3bd-allah <abdullah.nabil5044@gmail.com>
2024-08-05 05:45:06 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Header from './components/Header.jsx'
+import CalculatorForm from './components/CalculatorForm.jsx'
+import './index.css'
+
+function App() {
+  return (
+  <>
+    <Header />
+    <CalculatorForm />
+  </>
+
+  
+  )
+
+
+}
+
+export default App
---
+++ src/components/CalculatorForm.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+
+const CalculatorForm = () => {
+  return (
+    <>
+        <div id='user-input' className='input-group'>
+                <div>
+                    <label >Initial investment</label>
+                    <input type='number'/>
+                </div>
+                <br />
+                <div>
+                    <label >Initial investment</label>
+                    <input type='number' />
+                </div>
+                <br />
+                <div>
+                    <label >Initial investment</label>
+                    <input type='number'/>
+                </div>
+                <div>
+                    <label >Initial investment</label>
+                    <input type='number'/>
+                </div>
+        </div>
+    </>
+
+)
+}
+
+export default CalculatorForm
